4eccce44c4de96693d5526aa2136bb9452eaa3bb
Biswarup Dutta <biswarupdutta@Biswarups-MacBook-Air.local>
2024-11-14 14:31:40 +0530

    Updated program

--- calloc_program.c
+++ calloc_program.c
@@ -1,9 +1,12 @@
+// Stack , heap , meta memory (static -> stack) , (dynamic -> heap)
+// Declaring the header files
 #include<stdio.h>
 #include<stdlib.h>
-
+// the main function
 int main(void){
 
     int *arr = NULL;
+    // using the calloc function declaring the 5 bit of memory
     arr = (int *)calloc(5, sizeof(int));
 
     if(arr == NULL){
@@ -13,18 +16,17 @@ int main(void){
     for(int i = 0; i < 5; i++){
         printf("%d ",arr[i]);
     }
-
+    // Declaring teh reallock function
     arr = (int *)realloc(arr, 10 * sizeof(int));
-
+    // Checking that if the memory allocation is failed or not ??
     if(arr == NULL){
         printf("Memory reallocation failed using realloc. \n");
     }
-
+    // Displaying the value and initializing the value......
     for(int i = 5; i < 10; arr[i++] = i + 1);
 
     for(int i = 0; i < 10; i++){
         printf("%d ",arr[i]);
     }
-
     
 }
