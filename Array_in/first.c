312834d402a587dee97b4f82870bf484430c44d6
Biswarup Dutta <biswarupdutta@Biswarups-MacBook-Air.local>
2024-11-19 19:53:35 +0530

    This is the first program of array

---
+++ Array_in/first.c
@@ -0,0 +1,29 @@
+// Passing the array as argument
+
+#include<stdio.h>
+
+// Creating another function..
+void update(int marks[], int size){
+    for(int i = 0; i < size; i++){
+        marks[i] += 1;
+    }
+}   
+
+//Declaring the main function
+int main(void){
+
+    // Creating the array....
+    int marks[] = {97, 98, 99};
+
+    int size = sizeof(marks) / sizeof(marks[0]);
+
+    // Calling the update function
+    update(marks, size);
+
+    // print out marks.....
+    for(int i = 0; i < size; i++){
+        printf("%d\n",marks[i]);
+    }
+
+return 0; // Nothing
+}
