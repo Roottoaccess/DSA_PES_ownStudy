1978ac7f0e0b99e4ac96827b4fbf871995a761f0
Biswarup Dutta <biswarupdutta@Biswarups-MacBook-Air.local>
2024-11-28 23:52:41 +0530

    dsa continue..

--- understanding_linklist.c
+++ understanding_linklist.c
@@ -1,114 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
-// creating the structure
+
 typedef struct node{
     int data;
     struct node *link;
 }*node;
 
-// creating the node
 node create_node(){
     node n = (node) malloc (sizeof(node));
 
     if(n == NULL){
-        printf("Memory allocation failed !");
+        printf("Memory allocation failed");
     }
 
-    int data; printf("Enter the data: "); scanf("%d",&n->data);
-
+    int data; printf("Enter the data? "); scanf("%d",&n->data);
     n -> link = NULL;
 
     return n;
 }
 
-// void display function
-void display(node n){
-    if(n == NULL){
-        printf("There is no node present in the memory !");
-        // return NULL;
-    }
-    printf("HEAD -> ");
-    while(n != NULL){
-        printf("%d -> ",n->data);
-
-        n = n -> link;
-    }
-    printf("NULL \n");
-}
-
-// insert in front function()
-node inst_in_front(node n){
-    if(n == NULL){
-        printf("There is no node present in the memory !");
-        // return NULL;
-    }
+node ins_at_front(node n){
     node new_node = create_node();
 
-    new_node -> link = n;
+    if(new_node == NULL){
+        printf("Node is not created successfully !");
+    }
 
+    new_node -> link = n;
     return new_node;
 }
 
-// insert in last
-node inst_in_last(node n){
-    if(n == NULL){
-        printf("There is no node present in the memory !");
-        // return NULL;
-    }
-
+node ins_at_last(node n){
     node new_node = create_node();
-
     node temp = n;
     while(temp -> link != NULL){
         temp = temp -> link;
     }
-
     temp -> link = new_node;
 
     return n;
 }
 
-// insert in position
-node inst_in_pos(node n){
-    if(n == NULL){
-        printf("There is no node present in the memory !");
-        // return NULL;
-    }
+node ins_at_pos(node n){
     node new_node = create_node();
 
-    int pos; printf("Enter the position: "); scanf("%d",&pos);
-    node temp = n;
+    int pos; printf("Enter the position? "); scanf("%d",&pos);
     if(pos == 0){
-        new_node-> link = n;
-        return new_node;
+        return ins_at_front(n);
     }
+    node temp = n;
     for(int i = 0; i < pos - 1; i++){
         temp = temp -> link;
     }
-
     new_node -> link = temp -> link;
     temp -> link = new_node;
 
     return n;
 }
 
-// delete in front
 node delete_front(node n){
     if(n == NULL){
-        printf("There is no node present to delete....");
+        printf("There is no node present in the memory to delete");
     }
     node temp = n;
-
     n = n -> link;
 
     free(temp);
     return n;
 }
 
-// delete in rare
 node delete_rare(node n){
     if(n == NULL){
-        printf("There is no node present to delete....");
+        printf("There is no node present in the memory to delete");
     }
     node temp = n;
 
@@ -118,52 +81,55 @@ node delete_rare(node n){
 
     node p = temp -> link;
 
-    temp -> link= NULL;
+    temp -> link = NULL;
 
     free(p);
 
     return n;
 }
 
-// delete in position
-node delete_position(node n){
+node delete_pos(node n){
     if(n == NULL){
-        printf("There is no node present to delete....");
+        printf("There is no node present in the memory to delete");
     }
-
     node temp = n;
-    int pos; printf("Enter the position to delete: "); scanf("%d",&pos);
+    int pos; printf("Enter the position to delete? "); scanf("%d",&pos);
+
     if(pos == 0){
-            return delete_front(n);
-        }
+        return delete_front(n);
+    }
     for(int i = 0; i < pos - 1; i++){
         temp = temp -> link;
     }
-
     node q = temp -> link;
-
-    temp -> link = temp -> link -> link;
-
     free(q);
+    temp -> link = temp -> link -> link;
 
     return n;
 }
 
-int main(void){
+void display(node n){
+    printf("HEAD -> ");
+    while(n != NULL){
+        printf("%d -> ", n -> data);
+        n = n -> link;
+    }
+    printf("NULL \n");
+}
 
-    // calling the functions
+int main(void){
     node n = create_node();
     display(n);
-    n = inst_in_front(n);
+    n = ins_at_front(n);
     display(n);
-    n = inst_in_last(n);
+    n = ins_at_last(n);
     display(n);
-    // n = inst_in_pos(n);
-    // display(n);
-    // n = delete_front(n);
-    // display(n);
-    // n = delete_rare(n);
-    // display(n);
-    n = delete_position(n);
+    n = ins_at_pos(n);
+    display(n);
+    n = delete_front(n);
+    display(n);
+    n = delete_rare(n);
+    display(n);
+    n = delete_pos(n);
     display(n);
 }
